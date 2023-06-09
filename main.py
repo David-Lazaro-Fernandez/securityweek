e140cd03b490a73207189148475d9a6f249d46cd
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-06-09 04:51:21 +0000

    Asymmetric Encryption 👍

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import hashlib
+
+
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.fernet import Fernet
+
 # with open("apache_access.bib", "r") as file:
 #     for line in file: 
 #         elements = line.split(" ")
@@ -54,33 +60,94 @@ print(secret_hash.hexdigest())
 
 # compare_files(file1, file2)
 
+# def generate_key():
+#     """Generate a new encryption key."""
+#     return Fernet.generate_key()
 
-from cryptography.fernet import Fernet
+# def encrypt_message(message, key):
+#     """Encrypt a message using the provided key."""
+#     f = Fernet(key)
+#     encrypted_message = f.encrypt(message.encode())
+#     return encrypted_message
 
-def generate_key():
-    """Generate a new encryption key."""
-    return Fernet.generate_key()
+# def decrypt_message(encrypted_message, key):
+#     """Decrypt an encrypted message using the provided key."""
+#     f = Fernet(key)
+#     decrypted_message = f.decrypt(encrypted_message)
+#     return decrypted_message.decode()
 
-def encrypt_message(message, key):
-    """Encrypt a message using the provided key."""
-    f = Fernet(key)
-    encrypted_message = f.encrypt(message.encode())
-    return encrypted_message
+# # Usage example
+# encryption_key = generate_key()
+# message_to_encrypt = "Hello, this is a secret message!"
 
-def decrypt_message(encrypted_message, key):
-    """Decrypt an encrypted message using the provided key."""
-    f = Fernet(key)
-    decrypted_message = f.decrypt(encrypted_message)
-    return decrypted_message.decode()
+# encrypted = encrypt_message(message_to_encrypt, encryption_key)
 
-# Usage example
-encryption_key = generate_key()
-message_to_encrypt = "Hello, this is a secret message!"
+# print("Key", encryption_key)
+# print("Encrypted message:", encrypted)
 
-encrypted = encrypt_message(message_to_encrypt, encryption_key)
+# decrypted = decrypt_message(encrypted, encryption_key)
+# print("Decrypted message:", decrypted)
 
-print("Key", encryption_key)
-print("Encrypted message:", encrypted)
 
-decrypted = decrypt_message(encrypted, encryption_key)
-print("Decrypted message:", decrypted)
+
+# # Serialize and save the private key
+# pem = private_key.private_bytes(
+#     encoding=serialization.Encoding.PEM,
+#     format=serialization.PrivateFormat.PKCS8,
+#     encryption_algorithm=serialization.NoEncryption()
+# )
+# with open('private_key.pem', 'wb') as f:
+#     f.write(pem)
+
+# # Serialize and save the public key
+# pem = public_key.public_bytes(
+#     encoding=serialization.Encoding.PEM,
+#     format=serialization.PublicFormat.SubjectPublicKeyInfo
+# )
+# with open('public_key.pem', 'wb') as f:
+#     f.write(pem)
+
+# Generate RSA key pair
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+public_key = private_key.public_key()
+
+# Save private key
+with open('private_key.pem', 'wb') as f:
+    f.write(private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ))
+
+# Load private key
+with open('private_key.pem', 'rb') as f:
+    private_key = serialization.load_pem_private_key(
+        f.read(),
+        password=None
+    )
+
+data = b"Hello, this is a secret message!"
+ciphertext = public_key.encrypt(
+    data,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+# Decrypt ciphertext using the private key
+decrypted_data = private_key.decrypt(
+    ciphertext,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+
+print("Decrypted data:", decrypted_data.decode())
